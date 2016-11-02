9d7a695217ad9faed442ec8e888625bf2b35b588
MAOUCHE <maouche.oussama@gmail.com>
2016-11-02 13:10:56 +0100

    change

--- app/src/main/java/first/maouche/studio_github/MainActivity.java
+++ app/src/main/java/first/maouche/studio_github/MainActivity.java
@@ -1,7 +1,9 @@
 package first.maouche.studio_github;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"sami",Toast.LENGTH_LONG).show();
+        System.out.print("no good");
     }
 }
